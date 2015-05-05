345846adfab650556e5f7ba9170990d93998fb9d
quynhthuyen <quynhthuyentruong@outlook.com.vn>
2015-05-05 17:43:36 +0700

    udpate

--- app/models/spree/address_decorator.rb
+++ app/models/spree/address_decorator.rb
@@ -1,12 +1,4 @@
 Spree::Address.class_eval do
-
 	clear_validators!
-
 	validates :address1, :city,:phone, :user_name, :district, presence: true
-
-
-	# _validators.reject!{ |key, _| key == :firstname }
-
-	# _validate_callbacks.reject! do |callback|
-	# 	callback.raw_filter.attributes == [:firstname]
-	end
+end
